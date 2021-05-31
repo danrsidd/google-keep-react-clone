e40dd84c45dc52ed9b0f4eba6b444dc0350711cf
Danyal Siddiqui <danrsidd@gmail.com>
2021-05-31 16:59:23 -0400

    Complete App Component Logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Form from "./Form";
 
 const App = () => {
-	return <div></div>;
+	const [notesCollection, setNotesCollection] = useState([]);
+
+	const addNote = (noteObj) => {
+		setNotesCollection((prevNotesCollection) => {
+			return [...prevNotesCollection, noteObj];
+		});
+	};
+
+	const deleteNote = (id) => {
+		setNotesCollection((prevNotesCollection) => {
+			return prevNotesCollection.filter((prevNoteObj) => {
+				return prevNoteObj.id != id;
+			});
+		});
+	};
+
+	return (
+		<div>
+			<Header />
+			<Form onAdd={addNote} />
+			{notesCollection.map((noteObj) => {
+				return (
+					<Note
+						key={noteObj.id}
+						id={noteObj.id}
+						content={noteObj.content}
+						onDelete={deleteNote}
+					/>
+				);
+			})}
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { v4 as uuidv4 } from uuid
+import { v4 as uuidv4 } from "uuid";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
 
@@ -10,16 +10,16 @@ const Form = (props) => {
 	});
 
 	const noteInputChange = (e) => {
-		const {value} = e.target;
+		const { value } = e.target;
 		setNote((prevNote) => {
 			return {
 				...prevNote,
 				// random id generator
 				id: uuidv4(),
-				content: value
+				content: value,
 			};
 		});
-	}
+	};
 
 	const addNote = (e) => {
 		// prevent form auto-refresh
@@ -27,28 +27,28 @@ const Form = (props) => {
 		props.onAdd(note);
 		// Clear input after submission
 		setNote({
-			id:"",
-			content:""
-		})
-	}
+			id: "",
+			content: "",
+		});
+	};
 
-	return <div>
-		<form className="create-note">
+	return (
+		<div>
+			<form className="create-note">
+				<textarea
+					name="content"
+					onChange={noteInputChange}
+					value={note.content}
+					placeholder="Type your note here..."
+					rows="3"
+				/>
 
-        <textarea
-          name="content"
-          onChange={noteInputChange}
-          value={note.content}
-          placeholder="Type your note here..."
-          rows="3"
-        />
-
-		<Fab onClick={submitNote}>
-			<AddIcon />
-		</Fab>
-
-      </form>
-	</div>;
+				<Fab onClick={addNote}>
+					<AddIcon />
+				</Fab>
+			</form>
+		</div>
+	);
 };
 
 export default Form;

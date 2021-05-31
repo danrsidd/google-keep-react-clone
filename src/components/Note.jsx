f8d25ab57acd2a1b39e78360219e32a8f16fbb70
Danyal Siddiqui <danrsidd@gmail.com>
2021-05-31 16:39:16 -0400

    Complete Note Component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 
-const Note = () => {
-	return <div></div>;
+const Note = (props) => {
+	const deleteClick = () => props.onDelete(props.id);
+
+	return (
+		<div className="note">
+			<p>{props.content}</p>
+			<button onClick={deleteClick}>
+				<HighlightOffIcon />
+			</button>
+		</div>
+	);
 };
 
 export default Note;

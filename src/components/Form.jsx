f1210240942ff87488a749ddd00df225775b7cf4
Danyal Siddiqui <danrsidd@gmail.com>
2021-05-31 16:48:16 -0400

    Complete Form Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from uuid
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
 
-const Form = () => {
-	return <div></div>;
+const Form = (props) => {
+	const [note, setNote] = useState({
+		id: "",
+		content: "",
+	});
+
+	const noteInputChange = (e) => {
+		const {value} = e.target;
+		setNote((prevNote) => {
+			return {
+				...prevNote,
+				// random id generator
+				id: uuidv4(),
+				content: value
+			};
+		});
+	}
+
+	const addNote = (e) => {
+		// prevent form auto-refresh
+		e.preventDefault();
+		props.onAdd(note);
+		// Clear input after submission
+		setNote({
+			id:"",
+			content:""
+		})
+	}
+
+	return <div>
+		<form className="create-note">
+
+        <textarea
+          name="content"
+          onChange={noteInputChange}
+          value={note.content}
+          placeholder="Type your note here..."
+          rows="3"
+        />
+
+		<Fab onClick={submitNote}>
+			<AddIcon />
+		</Fab>
+
+      </form>
+	</div>;
 };
 
 export default Form;
